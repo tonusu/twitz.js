6804e15a47f77f65955bc7dfe0416f82711692c2
Tonusu <psitosm@gmail.com>
2016-06-28 01:20:16 +0300

    0.4

--- js/twitz.setup.js
+++ js/twitz.setup.js
@@ -5,8 +5,8 @@
 // Edit variables below
 /************************************************************/
 
-var listlink = 'https://twitter.com/_tonusu/lists/twitz.js'; //EDIT THIS. the url of your list
-var widgetid = '298160141411418112'; //EDIT THIS. the id of your list
+var listlink = https://twitter.com/ExampleTwitterUsername/ExamplePublicTwitterListName; //EDIT THIS. the url of your list
+var widgetid = '123456789012345678'; //EDIT THIS. the id of your list
 
 var TwitzContainer = 'twitz-container1'; // the container you want to load the twitz in. They will be appended
 var linkcolor = '#ff0000'; //the color of links in the widget
